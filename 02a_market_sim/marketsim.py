02f4bdf6b1d9b67ea2a4ba8aebfcc4d61aa34592
Trang Nguyen <ntrang086@gmail.com>
2017-12-17 10:44:25 -0500

    feat: Complete the implementation of compute_portvals
    Update df_trades with transaction cost, i.e. commission + impact * value of stocks traded
    Compute df_value = df_prices * df_holdings
    Create and return portvals dataframe which contains the sum of each row in df_value

--- 02a_market_sim/marketsim.py
+++ 02a_market_sim/marketsim.py
@@ -18,53 +18,55 @@ def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000, c
     portvals: A dataframe with one column containing the value of the portfolio for each trading day
     """
 
-    # In the template, instead of computing the value of the portfolio, we just
-    # read in the value of IBM over 6 months
-    #portvals = pd.DataFrame.from_csv(orders_file)
+    # Read in the orders_file and sort it by date
     orders_df = pd.read_csv(orders_file, index_col='Date', parse_dates=True, na_values=['nan'])
-
-    # Sort the orders_df by index, i.e the date column
     orders_df.sort_index(ascending=True, inplace=True)
     
+    # Get the start and end dates and symbols
     start_date = orders_df.index.min()
     end_date = orders_df.index.max()
     symbols = orders_df.Symbol.unique()
 
-    # Create a dataframe with adjusted close prices for the symbols
+    # Create a dataframe with adjusted close prices for the symbols and for cash
     df_prices = get_data(symbols, pd.date_range(start_date, end_date), addSPY=False)
     df_prices.dropna(inplace=True)
-
-    # Add a column for adjusted close price for cash, which is $1
     df_prices["cash"] = 1.0
-    #print (df_prices)
 
     # Create a dataframe that represents changes in the number of shares by day for each asset. 
     # It has the same structure as df_prices, and is initially filled with zeros
     df_trades = pd.DataFrame(np.zeros((df_prices.shape)), df_prices.index, df_prices.columns)
-    #print (orders_df)
     for index, row in orders_df.iterrows():
+        # Total value of shares purchased or sold
+        traded_share_value = df_prices.loc[index, row["Symbol"]] * row["Shares"]
+        # Transaction cost 
+        transaction_cost = commission + impact * traded_share_value
+
+        # Update the number of shares and cash based on the type of transaction done
         if row["Order"] == "BUY":
-            df_trades.loc[index, row["Symbol"]] = row["Shares"]
-            df_trades.loc[index, "cash"] = df_prices.loc[index, row["Symbol"]] * row["Shares"] * (-1.0)
+            df_trades.loc[index, row["Symbol"]] = row["Shares"]            
+            df_trades.loc[index, "cash"] = traded_share_value * (-1.0) - transaction_cost
         else:
             df_trades.loc[index, row["Symbol"]] = -row["Shares"]
-            df_trades.loc[index, "cash"] = df_prices.loc[index, row["Symbol"]] * row["Shares"]
+            df_trades.loc[index, "cash"] = traded_share_value - transaction_cost
 
-    # Create a dataframe that represents on each particular day how much of each asset we are holding
+    # Create a dataframe that represents on each particular day how much of each asset in the portfolio
     # It has the same structure as df_prices, and is initially filled with zeros
     df_holdings = pd.DataFrame(np.zeros((df_prices.shape)), df_prices.index, df_prices.columns)
     for row_count in range(len(df_holdings)):
+        # In the first row, the shares are the same as in df_trades, but start_val must be added to cash
         if row_count == 0:
             df_holdings.iloc[0, :-1] = df_trades.iloc[0, :-1].copy()
             df_holdings.iloc[0, -1] = df_trades.iloc[0, -1] + start_val
+        # The rest of the rows show cumulative values
         else:
             df_holdings.iloc[row_count] = df_holdings.iloc[row_count-1] + df_trades.iloc[row_count]
         row_count += 1
 
-    # Dummy portvals
-    portvals = pd.DataFrame(index=df_prices.index, data=df_prices.iloc[:, 0].as_matrix())
-    print (portvals)
+    # Create a dataframe that represents the monetary value of each asset in the portfolio
+    df_value = df_prices * df_holdings
 
+    # Create portvals dataframe
+    portvals = pd.DataFrame(df_value.sum(axis=1), df_value.index)
     return portvals
 
 
