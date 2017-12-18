54a0e6187ed8178029e7e53a1c62bca6fd5677d7
Trang Nguyen <ntrang086@gmail.com>
2017-12-18 17:38:18 -0500

    test: Add a test using orders.csv

---
+++ 02a_market_sim/test_marketsim.py
@@ -0,0 +1,31 @@
+"""Test for optimization.py"""
+
+
+from marketsim import compute_portvals
+import unittest
+import math
+import pandas as pd
+from analysis import get_portfolio_stats
+
+
+class TestMarketSimWithOrders(unittest.TestCase):
+
+    def test_compute_portvals(self):
+        orders_file = "./orders/orders.csv"
+        portvals = compute_portvals(orders_file)
+
+        # Check if portvals is a dataframe
+        self.assertTrue(isinstance(portvals, pd.DataFrame))
+
+        # Test portfolio stats
+        cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals, daily_rf=0.0, samples_per_year=252.0)
+        self.assertTrue(math.isclose(cum_ret, 0.108872698544, rel_tol=0.02), "Cumulative return is incorrect")
+        self.assertTrue(math.isclose(avg_daily_ret, 0.000459098655493, rel_tol=0.02), "Average daily return is incorrect")
+        self.assertTrue(math.isclose(std_daily_ret, 0.00730509916835, rel_tol=0.02), "Standard deviation is incorrect")
+        self.assertTrue(math.isclose(sharpe_ratio, 0.997654521878, rel_tol=0.02), "Sharpe ratio is incorrect")
+        self.assertTrue(math.isclose(portvals.iloc[-1, -1], 1106025.8065, rel_tol=0.02), "Portfolio value is incorrect")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
