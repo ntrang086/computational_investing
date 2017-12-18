1caa30d6542d2a33c9d6de6f1c57a49dfc092625
Trang Nguyen <ntrang086@gmail.com>
2017-12-18 17:39:05 -0500

    test: Add 2nd test for compute_portvals using orders2.csv

--- 02a_market_sim/test_marketsim.py
+++ 02a_market_sim/test_marketsim.py
@@ -26,6 +26,23 @@ class TestMarketSimWithOrders(unittest.TestCase):
         self.assertTrue(math.isclose(portvals.iloc[-1, -1], 1106025.8065, rel_tol=0.02), "Portfolio value is incorrect")
 
 
+class TestMarketSimWithOrders2(unittest.TestCase):
+
+    def test_compute_portvals(self):
+        orders_file = "./orders/orders2.csv"
+        portvals = compute_portvals(orders_file)
+
+        # Check if portvals is a dataframe
+        self.assertTrue(isinstance(portvals, pd.DataFrame))
+
+        # Test portfolio stats
+        cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals, daily_rf=0.0, samples_per_year=252.0)
+        self.assertTrue(math.isclose(cum_ret, 0.0538411196951, rel_tol=0.02), "Cumulative return is incorrect")
+        self.assertTrue(math.isclose(avg_daily_ret, 0.000253483085898, rel_tol=0.02), "Average daily return is incorrect")
+        self.assertTrue(math.isclose(std_daily_ret, 0.00728172910323, rel_tol=0.02), "Standard deviation is incorrect")
+        self.assertTrue(math.isclose(sharpe_ratio, 0.552604907987, rel_tol=0.02), "Sharpe ratio is incorrect")
+        self.assertTrue(math.isclose(portvals.iloc[-1, -1], 1051088.0915, rel_tol=0.02), "Portfolio value is incorrect")    
+    
 
 if __name__ == '__main__':
     unittest.main()
